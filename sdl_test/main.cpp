3ae38a80e1ff1d579e898e3e5d620501c86edf56
rmatttu <mggggk@gmail.com>
2017-07-26 20:18:20 +0900

    utf8、CRLFに書き換えテスト（？）

--- sdl_test/main.cpp
+++ sdl_test/main.cpp
@@ -6,6 +6,7 @@
 #include"SDL_ttf.h"
 #include"SDL_mixer.h"
 
+
 int main(int argc, char* args[]) {
 
 	SDL_Init(SDL_INIT_EVERYTHING);
@@ -24,6 +25,50 @@ int main(int argc, char* args[]) {
 	Mix_PlayMusic(music, -1);
 
 
+	if (TTF_Init() == -1) {
+		printf("TTF_Init: %s\n", TTF_GetError());
+		exit(2);
+	}
+	TTF_Font* Sans = TTF_OpenFont("res/ipag-mona.ttf", 72); //this opens a font style and sets a size
+	if (!Sans){
+		SDL_Log(SDL_GetError());
+		return 0;
+	}
+	SDL_Color White = { 255, 0, 0 };  // this is the color in rgb format, maxing out all would give you the color white, and it will be your text's color
+	SDL_Surface* surfaceMessage = TTF_RenderText_Solid(Sans, "put your text here", White); // as TTF_RenderText_Solid could only be used on SDL_Surface then you have to create the surface first
+	if (!surfaceMessage)return 0;
+	SDL_Texture* Message = SDL_CreateTextureFromSurface(render, surfaceMessage); //now you can convert it into a texture
+	if (!Message)return 0;
+	SDL_Rect Message_rect; //create a rect
+	Message_rect.x = 0;  //controls the rect's x coordinate 
+	Message_rect.y = 0; // controls the rect's y coordinte
+	Message_rect.w = 800; // controls the width of the rect
+	Message_rect.h = 100; // controls the height of the rect
+	//Mind you that (0,0) is on the top left of the window/screen, think a rect as the text's box, that way it would be very simple to understance
+	//Now since it's a texture, you have to put RenderCopy in your game loop area, the area where the whole code executes
+	//SDL_RenderCopy(render, Message, NULL, &Message_rect); //you put the renderer's name first, the Message, the crop size(you can ignore this if you don't want to dabble with cropping), and the rect which is the size and coordinate of your texture
+	//Don't forget too free your surface and texture
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // テストです
+
+
 	// 画像
 	SDL_Surface *image;
 	image = IMG_Load("res/bird_koruri.png");
@@ -33,14 +78,15 @@ int main(int argc, char* args[]) {
 		// ここでエラー処理を行う
 	}
 	SDL_Texture *texture;
-	texture = SDL_CreateTextureFromSurface(render, image);
+	texture  = SDL_CreateTextureFromSurface(render, image);
 
 	int volume = 128;
 	SDL_Event ev;
 	while (true) {
+		// 文字描画
+		SDL_RenderCopy(render, Message, NULL, &Message_rect); //you put the renderer's name first, the Message, the crop size(you can ignore this if you don't want to dabble with cropping), and the rect which is the size and coordinate of your texture
 		// 画像の描画
-		SDL_RenderCopy(render, texture, nullptr, nullptr);
-
+		SDL_RenderCopy(render, texture , nullptr, nullptr);
 		//線の描画
 		SDL_SetRenderDrawColor(render, 255, 0, 0, 255);
 		SDL_RenderDrawLine(render, 10, 10, 400, 400);
